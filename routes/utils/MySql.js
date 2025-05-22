7fbef9f007c17a3411a6684f6f46e388b276b895
Michael livshits <livshmic@post.bgu.ac.il>
2025-05-22 19:45:54 +0300

    server start commit - Now by running node ./main.js we can start the server and start sending requests to server

--- routes/utils/MySql.js
+++ routes/utils/MySql.js
@@ -4,11 +4,10 @@ require("dotenv").config();
 
 const config={
 connectionLimit:4,
-  host: process.env.host,//"localhost"
-  user: process.env.user,//"root"
-  password: process.env.DBpassword,
-  database:process.env.database
-  // database:"mydb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 }
 const pool = new mysql.createPool(config);
 
